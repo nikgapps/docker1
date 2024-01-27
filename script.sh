2e53868e79cf5be6a3b9bbb54af1b89436aaa4d0
Nikhil Menghani <nikhil@menghani.com>
2024-01-26 21:40:23 -0500

    Fix the way we execute the command

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@ GIT_USER_EMAIL=$2
 ANDROID_VERSION=$3
 PACKAGE_LIST=$4
 UPDATE_WEBSITE=$5
-[ "$UPDATE_WEBSITE" = "1" ] && UPDATE_WEBSITE="--updateWebsite" || UPDATE_WEBSITE=""
 
 # Configure git with the provided username and email
 git config --global http.postBuffer 157286400
@@ -15,4 +14,8 @@ git config --global user.email "$GIT_USER_EMAIL"
 
 echo "nikgapps --sign --upload --release --androidVersion $ANDROID_VERSION --packageList $PACKAGE_LIST $UPDATE_WEBSITE"
 # Now run nikgapps with the provided Android version and package list
-nikgapps --sign --upload --release --androidVersion $ANDROID_VERSION --packageList $PACKAGE_LIST $UPDATE_WEBSITE
+if [ "$UPDATE_WEBSITE" = "1" ]; then 
+    nikgapps --sign --upload --release --androidVersion $ANDROID_VERSION --packageList $PACKAGE_LIST --updateWebsite
+else
+    nikgapps --sign --upload --release --androidVersion $ANDROID_VERSION --packageList $PACKAGE_LIST
+fi
